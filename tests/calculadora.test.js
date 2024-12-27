1d3729ce97a89679ed35b53d8a9520201c45c44b
Gabriel Braico Dornas <gabrielbdornas@gmail.com>
2024-12-27 00:48:39 +0000

    cria teste de teste

---
+++ tests/calculadora.test.js
@@ -0,0 +1,11 @@
+test("nome do teste", () => {
+  console.log("esta função está sendo chamada?");
+});
+
+test("outro teste", () => {
+  console.log("esta função está sendo chamada 2?");
+});
+
+test("espero que 1 seja 1", () => {
+  expect(1).toBe(1);
+});
